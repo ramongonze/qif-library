8760559d462971b74d4ad077fa2ca49949991db4
Ramon G. Gonze <ramongonze@gmail.com>
2021-10-16 21:18:34 -0300

    Removed num_secrets from Secrets constructor because it can be inferred from the list of labels.

--- libqif/core/secrets.py
+++ libqif/core/secrets.py
@@ -1,33 +1,23 @@
 """Set of secrets."""
 
-from util.types import check_int, check_list
+from util.types import check_list
 from util.probability import check_prob_distribution
 from numpy import array
 
 class Secrets:
     
-    def __init__(self, num_secrets, labels, prob):
+    def __init__(self, secrets, prob):
         """Set of secrets.
 
         Parameters
         ----------
-        num_secrets : int
-            Number of secrets.
-
-        labels : list
+        secrets : set
             Secrets labels.
 
         prob : list, numpy.ndarray
             Array of probability distributions. prob[i] is the probability of
             secret with i-th label happen.
         """
-        self.num_secrets = check_int(num_secrets)
-        self.labels = self._check_labels(labels)
+        self.labels = check_list(secrets)
+        self.num_secrets = len(secrets)
         self.prob = array(check_prob_distribution(prob))
-
-    def _check_labels(self, labels):
-        """Check if the parameter is a list and has |labels| = number of secrets."""
-        check_list(labels)
-        if len(labels) != self.num_secrets:
-            raise Exception('The number of labels is different from the number of secrets')
-        return labels        
