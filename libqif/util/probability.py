9568eafa990898484a1af3d534c138c8f17d78c1
Ramon G. Gonze <ramongonze@gmail.com>
2021-10-16 21:29:01 -0300

    Removed the return from check_prob_distribution.

--- libqif/util/probability.py
+++ libqif/util/probability.py
@@ -6,7 +6,7 @@ def check_prob_distribution(prob):
     """Chech wheter the vector is a probability distribution or not.
     All the values must be in the interval [0,1] and they must sum up to 1.
     It raises an error if the array is not a probability distribution or
-    returns the input parameters if it is a valid probability distribution.
+    does nothing if it is a valid probability distribution.
     """
 
     epsilon = 0.000001 # Used to compare probability distributions
@@ -18,5 +18,3 @@ def check_prob_distribution(prob):
     if prob_sum < 1-epsilon or prob_sum > 1+epsilon:
         raise ValueError('All the values must sum up to 1 (with an error of ' +
                          'at most 1^(-6)')
-
-    return prob
