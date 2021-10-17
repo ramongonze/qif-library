ae29c0d317c53d985f1fb24a46d6de55782f7509
Ramon G. Gonze <ramongonze@gmail.com>
2021-10-16 21:22:53 -0300

    Add numpy array.

--- libqif/util/types.py
+++ libqif/util/types.py
@@ -1,5 +1,7 @@
 """Methods to check types of variables."""
 
+from numpy import array
+
 def check_int(value):
     if type(value) != type(1):
         raise TypeError('The value must be an integer')
@@ -33,5 +35,11 @@ def check_dict(value):
 def check_set(value):
     if type(value) != type(set()):
         raise TypeError('The value must be a set')
+    else:
+        return value
+
+def check_numpy_array(value):
+    if type(value) != type(array([])):
+        raise TypeError('The value must be a numpy array')
     else:
         return value
