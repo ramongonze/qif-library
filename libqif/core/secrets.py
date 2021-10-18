297701f57a2eb19ce941446f5d0e0fcc325f5b2c
Ramon G. Gonze <ramongonze@gmail.com>
2021-10-18 15:55:31 -0300

    Add functions to check types and sizes and updated functions names from libqif.util.types.

--- libqif/core/secrets.py
+++ libqif/core/secrets.py
@@ -1,6 +1,6 @@
 """Set of secrets."""
 
-from libqif.util.types import check_list
+from libqif.util.types import is_list, is_numpy_array
 from libqif.util.probability import check_prob_distribution
 from numpy import array
 
@@ -21,7 +21,6 @@ class Secrets:
             Prior distribution on the set of secrets. prior[i] is the
             probability of secret named labels[i] beeing the real secret.
 
-
         Parameters
         ----------
         secrets : list
@@ -31,15 +30,23 @@ class Secrets:
             Prior distribution on the set of secrets. prior[i] is the
             probability of secret named labels[i] beeing the real secret.
         """
-        self._check_labels_and_prior(secrets, prior)
-        self.labels = check_list(secrets)
-        self.num_secrets = len(secrets)
-        check_prob_distribution(prior)
+        self._check_types(secrets, prior)
+        self._check_sizes(secrets, prior)
+        self.labels = secrets.copy()
+        self.num_secrets = len(self.labels)
+        check_prob_distribution(prior) # Check if the array is a probability distribution
         self.prior = array(prior)
 
-    def _check_labels_and_prior(self, secrets, prior):
-        """Check if the size of the list of labels is the same of the prior
-        distribution.
+    def _check_types(self, secrets, prior):
+        if not is_list(secrets):
+            raise TypeError('The parameter \'secrets\' must be a list')
+
+        if not is_list(prior) and not is_numpy_array(prior):
+            raise TypeError('The parameter \'prior\' must be a list or a numpy.ndarray')
+
+    def _check_sizes(self, secrets, prior):
+        """Check if the size of the list of labels is the same of the 
+        number of elements in the prior distribution.
         """
         if len(secrets) != len(prior):
             raise Exception('The size of label\'s list is different from ' +
