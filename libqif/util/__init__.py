d718f5f128f5bb74834a7662d381bf468e7253d9
Ramon G. Gonze <ramongonze@gmail.com>
2021-10-18 16:11:47 -0300

    Updated functions names.

--- libqif/util/__init__.py
+++ libqif/util/__init__.py
@@ -1,10 +1,5 @@
 """Util methods."""
 
-from libqif.util.types import check_int
-from libqif.util.types import check_string
-from libqif.util.types import check_float
-from libqif.util.types import check_list
-from libqif.util.types import check_dict
-from libqif.util.types import check_set
-from libqif.util.types import check_numpy_array
+from libqif.util.types import is_int, is_float, is_string, is_list, is_dict, \
+    is_set, is_numpy_array, is_2d_list_matrix, is_2d_numpy_matrix, is_function
 from libqif.util.probability import check_prob_distribution
